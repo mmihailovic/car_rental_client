e7a17e194419096691dc588cf9ada6f5451b96a5
VikTheTrick <viktorvujicic1@gmail.com>
2023-01-21 03:52:59 +0100

    log/reg

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,11 +5,59 @@ import Header from '../components/Header';
 const LoginPage = () => {
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
+    const [token,setToken] = useState("");
+    const[loggedIn,setLogedIn] = useState(false)
     function handleUsername() {
         setUsername(document.getElementById("username").value);
     }
     function handlePassword() {
         setPassword(document.getElementById("password").value);
+    }
+
+   function handleRegister() {
+      var url = "http://localhost:8084/user-service/api/user/registerClient"
+      var method = "POST"
+      console.log("sss")
+      fetch(url, {
+          method: method,
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({"email":username, "password":password})
+  
+      })
+      .then(response => response.json())
+      .then(response => console.log(JSON.stringify(response))) 
+    }
+
+    function handleLogin() {
+      var url = "http://localhost:8084/user-service/api/user/login"
+      var method = "POST"
+      console.log("sss")
+      fetch(url, {
+          method: method,
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({"email":username, "password":password})
+  
+      })
+      .then(response => {
+        console.log(response)
+        if(response.ok)
+        {
+            response.json().then(res=>{
+              localStorage.setItem('token', res.token)
+              setToken(res.token)
+              console.log(res)
+                setLogedIn(true)
+            })
+        }
+        else
+        alert('Wrong credentials')
+    })
     }
     console.log(username);
     console.log(password);
@@ -18,8 +66,8 @@ const LoginPage = () => {
         <Header></Header>
         <p>Username</p> <input type="text" id="username" onChange={handleUsername}></input>
     <p>Password</p> <input type="password" id="password" onChange={handlePassword}></input>
-    <button>Login</button>
-    <button>Register as client</button>
+    <button onClick={handleLogin}>Login</button>
+    <button onClick={handleRegister}>Register as client</button>
     <button>Register as manager</button>
     </div>
   )
